7e47e73a4509cb95c2dd8817f6512ebefb917ccc
Tiollit <Tiollit7@gmail.com>
2022-10-01 20:04:36 +0200

    Lab 2 done 2

--- main.cpp
+++ main.cpp
@@ -5,11 +5,17 @@ using namespace std;
 int main() 
 {
 	double t1, t2, t3, t21, t22, t23;
-	cout << "Enter you begining time hours, minutes, seconds " << endl;
+	cout << "Enter you begining time - hours minutes seconds " << endl;
 	cin >> t1 >> t2 >> t3;
-	cout << "Enter you end time hours, minutes, seconds " << endl;
+	cout << "Enter you end time - hours minutes seconds " << endl;
 	cin >> t21 >> t22 >> t23;
-	cout << "You journey cost is " << ((60 * t21 + t22 + t23 / 60) - (60 * t1 + t2 + t3 / 60))*2 << " grn";
-
+	if (t1 >= 0 && t2 >= 0 && t2 <= 60 && t3>=0 && t3 <= 60)
+	{
+		cout << "You journey cost is " << ((60 * t21 + t22 + t23 / 60) - (60 * t1 + t2 + t3 / 60)) * 2 << " grn";
+	}
+	else
+	{
+		cout << "Entered wrong time" << endl;
+	}
 	return 0;
 }
