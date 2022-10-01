0c4ea3d676f0cd32176482a96c83ec1f9c498d0d
Tiollit <Tiollit7@gmail.com>
2022-10-01 20:22:08 +0200

    Some upgrades

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 	cin >> t1 >> t2 >> t3;
 	cout << "Enter you end time - hours minutes seconds " << endl;
 	cin >> t21 >> t22 >> t23;
-	if (t1 >= 0 && t2 >= 0 && t2 <= 60 && t3>=0 && t3 <= 60)
+	if (t1 >= 0 && t2 >= 0 && t2 <= 60 && t3 >=0 && t3 <= 60 && t21 >= 0 && t22 >= 0 && t22 <= 60 && t23 >= 0 && t23 <= 60 && ((60 * t21 + t22 + t23 / 60) - (60 * t1 + t2 + t3 / 60)) > 0)
 	{
 		cout << "You journey cost is " << ((60 * t21 + t22 + t23 / 60) - (60 * t1 + t2 + t3 / 60)) * 2 << " grn";
 	}
