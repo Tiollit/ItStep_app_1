77c6ef3605f1eff20b955872bff88cb91e700120
Tiollit <Tiollit7@gmail.com>
2022-10-01 18:21:30 +0200

    Lab 2 done

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int main() 
 {
+	double t1, t2, t3, t21, t22, t23;
+	cout << "Enter you begining time hours, minutes, seconds " << endl;
+	cin >> t1 >> t2 >> t3;
+	cout << "Enter you end time hours, minutes, seconds " << endl;
+	cin >> t21 >> t22 >> t23;
+	cout << "You journey cost is " << ((60 * t21 + t22 + t23 / 60) - (60 * t1 + t2 + t3 / 60))*2 << " grn";
 
-
-
+	return 0;
 }
