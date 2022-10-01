bd8c12faaa2f8cf7bc8e1502889f03c0eb8742ec
Tiollit <Tiollit7@gmail.com>
2022-10-01 18:01:18 +0200

    Lab 1 done normal

--- main.cpp
+++ main.cpp
@@ -2,9 +2,15 @@
 #include <cmath>
 using namespace std;
 
-int main() 
+int main()
 {
+	double s;
+	double t;
+	cout << "Enter distance in kilometers - s" << endl;
+	cin >> s;
+	cout << "Enter time in minutes - t" << endl;
+	cin >> t;
+	cout << "Speed must be " << 60 * s / t << " kilometers per hour" << endl;
 
-
-
+	return 0;
 }
